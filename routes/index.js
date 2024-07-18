3dbe91cc79cd80a5fafaf6e8028df7971c4242ec
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2024-07-18 18:50:17 +0530

    first commit

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+const indexRouter = express.Router();
+
+/* GET home page. */
+indexRouter.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+export default indexRouter;
